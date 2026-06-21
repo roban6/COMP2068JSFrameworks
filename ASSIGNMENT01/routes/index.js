beac508b8578eaa17b127945dfe8ae1b079587d4
roban6 <robankang34@gmail.com>
2026-06-21 18:08:18 +0300

    Add portfolio pages and Express routes

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Home',
+    pageTitle: 'Robanpreet Singh Portfolio'
+  });
+});
+
+/* About Me page */
+router.get('/about', function(req, res, next) {
+  res.render('about', {
+    title: 'About Me',
+    pageTitle: 'About Me'
+  });
+});
+
+/* Projects page */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', {
+    title: 'Projects',
+    pageTitle: 'Projects'
+  });
+});
+
+/* Contact Me page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {
+    title: 'Contact Me',
+    pageTitle: 'Contact Me'
+  });
 });
 
 module.exports = router;
