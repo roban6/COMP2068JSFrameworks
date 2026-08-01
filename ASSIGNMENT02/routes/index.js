032c5ba7639cfc49f9793d0cf2715f2a454fdc86
roban6 <robankang34@gmail.com>
2026-08-01 21:15:42 +0300

    Clean Express starter and create application layout

--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -3,12 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 
+
 /* GET home page */
 
 router.get('/', function(req, res, next) {
 
-  res.render('index', { 
+  res.render('index', {
+
     title: 'Video Game Collection Tracker'
+
   });
 
 });
